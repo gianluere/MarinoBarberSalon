1880ef31b577ab0888465200f5cffcce4a98a6f8
gianluere <gianlucaeremita.03@gmail.com>
2024-10-05 11:45:26 +0200

    Prima lista filtrata funzionante

---
+++ app/src/main/java/com/example/marinobarbersalon/ListaGiorniViewModel.kt
@@ -0,0 +1,211 @@
+package com.example.marinobarbersalon
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+class ListaGiorniViewModel : ViewModel() {
+
+    private val _listaGiorni = MutableStateFlow<List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>>(emptyList())
+    val listaGiorni: StateFlow<List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>> = _listaGiorni.asStateFlow()
+
+    private val _listaGiorniOccupati = MutableStateFlow<List<Pair<LocalDate, List<LocalTime>>>>(emptyList())
+    val listaGiorniOccupati : StateFlow<List<Pair<LocalDate, List<LocalTime>>>> = _listaGiorniOccupati.asStateFlow()
+
+    private val _listaGiorniAggiornata = MutableStateFlow<List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>>(emptyList())
+    val listaGiorniAggiornata: StateFlow<List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>> = _listaGiorniAggiornata.asStateFlow()
+
+    private val giorniFestivi = listOf(
+        LocalDate.of(2024, 1, 1),  // Capodanno
+        LocalDate.of(2024, 12, 25), // Natale
+        LocalDate.of(2024, 12, 26), // Santo Stefano
+        LocalDate.of(2024, 4, 25),  // Festa della Liberazione
+        LocalDate.of(2024, 8, 15)   // Ferragosto
+    )
+
+
+
+    init {
+            viewModelScope.launch {
+                try{
+                    // Genera la lista dei giorni
+                    _listaGiorni.value = generateListaDate(LocalDate.now(), 60, giorniFestivi)
+                    _listaGiorniAggiornata.value = generateListaDate(LocalDate.now(), 60, giorniFestivi)
+
+                    val prova = async{ generaListaOccupatiSospend(LocalDate.now(), 60)}
+                    _listaGiorniOccupati.value = prova.await()
+                    // Genera la lista occupati
+                    //_listaGiorniOccupati.value = generaListaOccupatiSospend(LocalDate.now(), 60)
+                    // Aggiorna la lista dei giorni con quelli occupati
+                    val lista = aggiornaListaOccupati(_listaGiorni.value, _listaGiorniOccupati.value)
+                    _listaGiorniAggiornata.value = lista
+
+                    Log.d("ViewModelInit", "Lista aggiornata: ${_listaGiorniAggiornata.value}")
+
+
+                    //_listaGiorniAggiornata.value =_listaGiorni.value
+                }catch (e: Exception){
+                    Log.e("ViewModelInit", "Errore durante l'inizializzazione: ${e.message}", e)
+                }
+            }
+
+
+
+    }
+
+
+    fun generateListaDate(
+        oggi: LocalDate,
+        giorniTotali: Int,
+        giorniFestivi: List<LocalDate>
+    ): List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>> {
+        val giorniDisponibili = mutableListOf<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>()
+
+        val orarioInizio = LocalTime.of(9, 0)
+        val orarioFine = LocalTime.of(20, 0)
+        val durataSlot = 30L
+
+        for (i in 0..giorniTotali) {
+            val giornoCorrente = oggi.plusDays(i.toLong())
+
+            // Escludi sabato, domenica e giorni festivi
+            if (giornoCorrente.dayOfWeek == DayOfWeek.SATURDAY ||
+                giornoCorrente.dayOfWeek == DayOfWeek.SUNDAY ||
+                giorniFestivi.contains(giornoCorrente)) {
+                // Aggiungi il giorno senza orari disponibili
+                giorniDisponibili.add(giornoCorrente to emptyList())
+            } else {
+                val slotOrari = mutableListOf<Pair<LocalTime, LocalTime>>()
+
+                var orarioCorrente = orarioInizio
+                while (orarioCorrente.isBefore(orarioFine)) {
+                    val orarioSuccessivo = orarioCorrente.plusMinutes(durataSlot)
+                    slotOrari.add(orarioCorrente to orarioSuccessivo)
+                    orarioCorrente = orarioSuccessivo
+                }
+
+                giorniDisponibili.add(giornoCorrente to slotOrari)
+            }
+        }
+
+        return giorniDisponibili
+    }
+
+
+
+    private suspend fun generaListaOccupatiSospend(
+        oggi: LocalDate,
+        giorniTotali: Int
+    ): List<Pair<LocalDate, List<LocalTime>>> {
+        val db = FirebaseFirestore.getInstance()
+        val listaOccupati = mutableListOf<Pair<LocalDate, List<LocalTime>>>()
+        val ultimo = oggi.plusDays(giorniTotali.toLong())
+
+        try {
+            // Chiamata Firestore bloccante con await()
+            val giorni = db.collection("occupati").get().await()
+
+            for (giorno in giorni) {
+                val dati = giorno.data
+                val giornoCorrente = LocalDate.parse(giorno.id, DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+
+                Log.d("ListaGiorniViewModel", "Elaborando il giorno: $giornoCorrente")
+                Log.d("ListaGiorniViewModel", giorno.id)
+
+                if (giornoCorrente.isEqual(oggi) ||
+                    (giornoCorrente.isAfter(oggi) && giornoCorrente.isBefore(ultimo)) ||
+                    giornoCorrente.isEqual(ultimo)
+                ) {
+                    val slotOrari = mutableListOf<LocalTime>()
+                    for (key in dati.keys) {
+                        Log.d("ListaGiorniViewModel", key)
+                        slotOrari.add(
+                            LocalTime.parse(
+                                key.toString(),
+                                DateTimeFormatter.ofPattern("HH:mm")
+                            )
+                        )
+                    }
+                    slotOrari.sort()
+                    listaOccupati.add(giornoCorrente to slotOrari)
+                }
+            }
+
+            Log.d("ListaGiorniViewModel", "Chiamata a Firestore per ottenere i giorni occupati")
+
+        } catch (e: Exception) {
+            Log.e("ListaGiorniViewModel", "Errore durante l'accesso a Firestore: ${e.message}", e)
+        }
+
+        Log.d("Stampa", listaOccupati.toString())
+
+        return listaOccupati
+    }
+
+
+    private fun aggiornaListaOccupati(
+        listaOrari: List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>,
+        listaOccupati: List<Pair<LocalDate, List<LocalTime>>>
+    ): List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>> {
+
+        // Crea una nuova lista modificata a partire dalla listaOrari
+        return listaOrari.map { (data, orariDisponibili) ->
+            Log.d("ListaGiorniViewModel", data.toString())
+            // Trova se esiste una corrispondenza nella listaOccupati per la data corrente
+            val occupatiPerQuestaData = listaOccupati.find { it.first == data }?.second
+
+
+            // Se ci sono orari occupati per questa data, rimuovili da orariDisponibili
+            val orariAggiornati = if (occupatiPerQuestaData != null) {
+                Log.d("ListaGiorniViewModel", "ciao")
+                /*orariDisponibili.filterNot {
+                    val isOcc = occupatiPerQuestaData.contains(it.first)
+                    Log.d("ListaGiorniViewModel", "Orario: ${it.first}, Occupato: $isOcc")
+                    isOcc
+                    //val isOccupato = occupatiPerQuestaData.contains(orarioInizio)
+                    //Log.d("ListaGiorniViewModel", "Orario: $orarioInizio, Occupato: $isOccupato")
+                    //isOccupato
+                }
+                 */
+                val slotOrari = mutableListOf<Pair<LocalTime, LocalTime>>()
+                var orarioCorrente = LocalTime.of(9,0)
+                while (orarioCorrente.isBefore(LocalTime.of(20,0))) {
+                    val orarioSuccessivo = orarioCorrente.plusMinutes(30)
+                    //Verifica se l'orario corrente è occupato confrontando solo ore e minuti
+                    val isOccupato = occupatiPerQuestaData.any { occupato ->
+                        occupato.hour == orarioCorrente.hour && occupato.minute == orarioCorrente.minute
+                    }
+
+                    if (!isOccupato) {
+                        slotOrari.add(orarioCorrente to orarioSuccessivo)
+                    }
+                    orarioCorrente = orarioSuccessivo
+                }
+                //Log.d("Stampa", slotOrari.toString())
+                slotOrari
+            } else {
+                Log.d("ListaGiorniViewModel", "Diverso")
+                orariDisponibili
+            }
+            Log.d("Stampa", data.toString())
+            Log.d("Stampa", orariAggiornati.toString())
+
+
+
+            // Restituisci la data con la nuova lista di orari aggiornati
+            data to orariAggiornati
+        }
+    }
+
+}
--- app/src/main/java/com/example/marinobarbersalon/SelezionaGiorno.kt
+++ app/src/main/java/com/example/marinobarbersalon/SelezionaGiorno.kt
@@ -1,5 +1,7 @@
 package com.example.marinobarbersalon
 
+import android.util.Log
+import android.widget.TextView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -8,37 +10,46 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.marinobarbersalon.ui.theme.myFont
+import com.example.marinobarbersalon.ui.theme.my_bordeaux
 import com.example.marinobarbersalon.ui.theme.my_gold
 import com.example.marinobarbersalon.ui.theme.my_yellow
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObjects
+import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import java.util.Locale
@@ -76,28 +87,138 @@ fun generaListaDate(dataInizio : LocalDate, dataFine : LocalDate): List<LocalDat
 val listaDate = generaListaDate(LocalDate.now(), LocalDate.now().plusMonths(2))
 
 
+
+fun generateListaDate(
+    oggi: LocalDate,
+    giorniTotali: Int,
+    giorniFestivi: List<LocalDate>
+): List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>> {
+    val giorniDisponibili = mutableListOf<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>()
+
+    val orarioInizio = LocalTime.of(9, 0)
+    val orarioFine = LocalTime.of(20, 0)
+    val durataSlot = 30L
+
+    for (i in 0..giorniTotali) {
+        val giornoCorrente = oggi.plusDays(i.toLong())
+
+        // Escludi sabato, domenica e giorni festivi
+        if (giornoCorrente.dayOfWeek == DayOfWeek.SATURDAY ||
+            giornoCorrente.dayOfWeek == DayOfWeek.SUNDAY ||
+            giorniFestivi.contains(giornoCorrente)) {
+            // Aggiungi il giorno senza orari disponibili
+            giorniDisponibili.add(giornoCorrente to emptyList())
+        } else {
+            val slotOrari = mutableListOf<Pair<LocalTime, LocalTime>>()
+
+            var orarioCorrente = orarioInizio
+            while (orarioCorrente.isBefore(orarioFine)) {
+                val orarioSuccessivo = orarioCorrente.plusMinutes(durataSlot)
+                slotOrari.add(orarioCorrente to orarioSuccessivo)
+                orarioCorrente = orarioSuccessivo
+            }
+
+            giorniDisponibili.add(giornoCorrente to slotOrari)
+        }
+    }
+
+    return giorniDisponibili
+}
+
+
+val giorniFestivi = listOf(
+    LocalDate.of(2024, 1, 1),  // Capodanno
+    LocalDate.of(2024, 12, 25), // Natale
+    LocalDate.of(2024, 12, 26), // Santo Stefano
+    LocalDate.of(2024, 4, 25),  // Festa della Liberazione
+    LocalDate.of(2024, 8, 15)   // Ferragosto
+)
+
+val giorni = generateListaDate(
+    LocalDate.now(),
+    60, giorniFestivi
+)
+
+
+/*
+fun generaListaOccupati(oggi: LocalDate,
+                        giorniTotali: Int)
+: List<Pair<LocalDate, List<Pair<LocalTime, LocalTime>>>>{
+    val db = Firebase.firestore
+
+    val listaOccupati = mutableListOf<Pair<LocalDate, List<LocalTime>>>()
+    val ultimo = oggi.plusDays(giorniTotali.toLong())
+
+
+    db.collection("appuntamenti")
+        .get()
+        .addOnSuccessListener { giorni ->
+            for (giorno in giorni) {
+                val dati = giorno.data
+                val giornoCorrente =
+                    LocalDate.parse(giorno.id, DateTimeFormatter.ofPattern("dd-mm-yyyy"))
+
+                if (giornoCorrente.isEqual(oggi) ||
+                    (giornoCorrente.isAfter(oggi) && giornoCorrente.isEqual(ultimo)) ||
+                    giornoCorrente.isEqual(ultimo)
+                ) {
+                    val slotOrari = mutableListOf<LocalTime>()
+                    for (key in dati.keys) {
+                        slotOrari.add(
+                            LocalTime.parse(
+                                key.toString(),
+                                DateTimeFormatter.ofPattern("HH:mm")
+                            )
+                        )
+
+                    }
+                    listaOccupati.add(giornoCorrente to slotOrari)
+                }
+
+
+            }
+
+        }
+
+
+    return listaOccupati
+}
+
+ */
+
+
 @Preview
 @Composable
 fun Data() {
 
-    var indexSelezionato by rememberSaveable {
+    val listaGiorniViewModel : ListaGiorniViewModel = viewModel()
+
+    val listaGiorni by listaGiorniViewModel.listaGiorniAggiornata.collectAsState()
+
+    var indexGiornoSelezionato by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    var indexOrarioSelezionato by rememberSaveable {
         mutableIntStateOf(0)
     }
 
     Column(modifier = Modifier
         .fillMaxSize()
-        .padding(top = 30.dp, bottom = 30.dp)) {
+        .padding(top = 30.dp)) {
         LazyRow(
             modifier = Modifier.background(color = Color(0xFFF5F5DC)),
             contentPadding = PaddingValues(25.dp),
             horizontalArrangement = Arrangement.spacedBy(23.dp)
         ) {
-            items(listaDate.size){index ->
+            items(listaGiorni.size){index ->
                 CardGiorno(
+                    giorno = listaGiorni[index].first,
                     index = index,
-                    isSelected = index == indexSelezionato,
+                    isSelected = index == indexGiornoSelezionato,
                     onCardSelected = { selectedIndex ->
-                        indexSelezionato = selectedIndex
+                        indexGiornoSelezionato = selectedIndex
+                        indexOrarioSelezionato = 0
                     }
                 )
 
@@ -107,29 +228,65 @@ fun Data() {
         Spacer(Modifier.height(30.dp))
 
         Box(modifier = Modifier
-            .padding(horizontal = 8.dp)){
+            .padding(horizontal = 8.dp)
+            .weight(1f)){
 
-            Box(modifier = Modifier.fillMaxSize()
+            Box(modifier = Modifier
+                .fillMaxSize()
                 .border(2.dp, my_gold, RoundedCornerShape(17.dp))
-                .background(color = my_yellow, RoundedCornerShape(17.dp))){
+                .background(color = my_yellow, RoundedCornerShape(17.dp)),
+                contentAlignment = Alignment.Center){
 
-                LazyColumn(
-                    contentPadding = PaddingValues(25.dp),
-                    verticalArrangement = Arrangement.spacedBy(23.dp)
-                ) {
-                    items(listaDate.size) { index ->
-                        CardOrario(
-                            index = index,
-                            isSelected = index == indexSelezionato,
-                            onCardSelected = { selectedIndex ->
-                                indexSelezionato = selectedIndex
-                            }
+                val orariDisponibili = listaGiorni[indexGiornoSelezionato].second
+
+                if (orariDisponibili.isEmpty()){
+                    Text(text = "NESSUN ORARIO DISPONIBILE\n" +
+                            "\n" +
+                            "SELEZIONARE UN’ALTRA DATA",
+                        fontSize = 23.sp,
+                        fontFamily = myFont,
+                        color = Color.Black,
+                        lineHeight = 14.sp
                         )
+                }else{
+                    LazyColumn(
+                        contentPadding = PaddingValues(25.dp),
+                        verticalArrangement = Arrangement.spacedBy(23.dp)
+                    ) {
+                        items(orariDisponibili.size) { index ->
+                            CardOrario(
+                                orario = orariDisponibili[index],
+                                index = index,
+                                indexGiornoSelezionato = indexGiornoSelezionato,
+                                isSelected = index == indexOrarioSelezionato,
+                                onCardSelected = { selectedIndex ->
+                                    indexOrarioSelezionato = selectedIndex
+                                }
+                            )
 
+                        }
                     }
                 }
 
+
+
             }
+
+
+        }
+        
+        Spacer(modifier = Modifier.height(25.dp))
+        Button(onClick = {
+            Log.d("Prova", listaGiorni[5].second.size.toString())
+            Log.d("Prova", listaGiorni[5].first.toString())
+            Log.d("Prova", listaGiorni[6].second.size.toString())
+            Log.d("Prova", listaGiorni[7].second.size.toString())
+        },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 9.dp, start = 14.dp, end = 14.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = my_bordeaux)) {
+            Text(text = "PROSEGUI", color = my_gold, fontFamily = myFont, fontSize = 25.sp)
         }
 
 
@@ -140,8 +297,8 @@ fun Data() {
 
 
 @Composable
-fun CardGiorno(index : Int, isSelected: Boolean, onCardSelected: (Int) -> Unit) {
-    val giorno = listaDate[index]
+fun CardGiorno(giorno : LocalDate, index : Int, isSelected: Boolean, onCardSelected: (Int) -> Unit) {
+
     Card(
         modifier = Modifier
             .size(90.dp)
@@ -182,9 +339,9 @@ fun CardGiorno(index : Int, isSelected: Boolean, onCardSelected: (Int) -> Unit) 
 
 
 @Composable
-fun CardOrario(index: Int, isSelected: Boolean, onCardSelected: (Int) -> Unit) {
+fun CardOrario(orario : Pair<LocalTime, LocalTime>, index: Int, indexGiornoSelezionato : Int, isSelected: Boolean, onCardSelected: (Int) -> Unit) {
 
-    val giorno = listaDate[index]
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -205,7 +362,8 @@ fun CardOrario(index: Int, isSelected: Boolean, onCardSelected: (Int) -> Unit) {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ){
-            Text(text = giorno.format(DateTimeFormatter.ofPattern("MMMM", Locale("it"))).replaceFirstChar { it.uppercase() },
+            val stringa = orario.first.toString() + " - " + orario.second.toString()
+            Text(text = stringa,
                 fontFamily = myFont,
                 color = Color.Black,
                 fontSize = 17.sp)
